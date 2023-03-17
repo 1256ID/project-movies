336e28f45507d00ce989627c4d1d084f53a3fa3d
TheAbsintheDuck <gabriel-carlsson@hotmail.com>
2023-03-17 11:15:11 +0100

    library

--- main.js
+++ main.js
@@ -3,34 +3,24 @@ Vue.createApp({
         return {
             search: "",
             movies: [],
+            topMovies:[],
             searchResults: [],
             jsonData: null
         };
     },
     created() {
-        fetch('.json')
+        fetch('movieLibrary.json')
             .then(response => response.json())
             .then(json => {
                 this.jsonData = json;
             });
     },
     methods: {
-        movieLibrary() {
+        topMovies() {
+            
+        },
 
-            // const regexPattern = new RegExp(this.search.split("").join(".*?"), "i");
-            // this.searchResults = [];
-
-            // if (this.search === "") {
-            //     return;
-            // }
-
-            // for (let i = 0; i < movieGenreArrays.length; i++) {
-            //     const movie = movieGenreArrays[i];
-
-            //     if (regexPattern.test(movie)) {
-            //         this.searchResults.push(movie);
-            //     }
-            // }
+        searchMovies() {
 
         }
     }
